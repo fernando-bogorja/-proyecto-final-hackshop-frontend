ca42d2905977b09041cfd077c6c479de8b585895
fedegorriaran <fedegorriaran@outlook.es>
2022-06-24 18:44:49 -0300

    agregar una llamada a la api asincrona en <Product/> usando useState y useEffect

--- front-hack-shop/src/components/Miscellaneous/DashBoard/product/Product.jsx
+++ front-hack-shop/src/components/Miscellaneous/DashBoard/product/Product.jsx
@@ -1,20 +1,32 @@
 import { useGetSingleProductQuery } from "../../../../redux/api-requests/products-req";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-function Product({ product }) {
+function Product({ param }) {
   const { productId } = useParams();
+  //-------------- Main Constants
+  const [product, setProduct] = useState([]);
+  //const [rating, setRating] = useState(0);
 
-  // dispatch(useGetAllProductsQuery());
+  //------- Sin Axios
+  const productFetch = async () => {
+    const data = await fetch(
+      "http://localhost:3001/api/products/get?_id=" + productId
+    );
+    const productData = await data.json();
+    setProduct(data);
+  };
 
-  // console.log(useGetSingleProductQuery().data);
-  /*
-  const { data, error, isLoading } = useGetSingleProductQuery();
+  useEffect(() => {
+    productFetch();
+    console.log(product);
+    console.log("param", param);
+  }, []);
 
-  console.log(data.data.name);
-*/
   return (
     <div style={{ marginTop: 100 }}>
       {/* // <h2>{data.data.name}</h2> */}
+      <h1>{product.name}</h1>
       <p>milanesa</p>
     </div>
   );
