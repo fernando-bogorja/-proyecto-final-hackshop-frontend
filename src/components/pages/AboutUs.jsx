2c8d0489c58ae2b5c9422a3e0c5e8aaa4aabd2d3
Fernando Bogorja <fbogorja@gmail.com>
2022-07-06 23:23:55 -0300

    haciendo responsive el aboutUs

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -17,7 +17,6 @@ const AboutUs = () => {
         height: "100%",
       }}
     >
-      {" "}
       <Box
         py={20}
         display="flex"
@@ -43,12 +42,17 @@ const AboutUs = () => {
         >
           ¡Sobre este proyecto!
         </Typography>
-        <Box width="80%" display="flex" justifyContent="space-between">
+        <Box
+          width="80%"
+          display="flex"
+          justifyContent="space-between"
+          border="4px solid red"
+        >
           <PeopleCard
             name="Lucía Rodriguez"
             image={require("../../assets/linkedin-lu.png")}
-            linkedin=""
-            github=""
+            linkedin="lurodriguezroldan"
+            github="LuuRoldan"
           />
           <PeopleCard
             name="Felipe Fontana"
@@ -58,9 +62,9 @@ const AboutUs = () => {
           />
           <PeopleCard
             name="Federico Gorriaran"
-            image={require("../../assets/linkedin-profile-example-3.png")}
-            linkedin=""
-            github=""
+            image={require("../../assets/linkedin-fede.png")}
+            linkedin="federico-gorriaran/"
+            github="fedegorriaran"
           />
           <PeopleCard
             name="Fernando Bogorja"
@@ -70,19 +74,10 @@ const AboutUs = () => {
           />
         </Box>
       </Box>
-      <Box sx={{ width: "80%" }}>
-        <Grid container spacing={5}>
-          <Grid item xs={12} my={20}>
-            <Box
-              className="box-header"
-              width="100%"
-              display="flex"
-              justifyContent="center"
-              flexWrap="wrap"
-              borderRadius="10px"
-            ></Box>
-          </Grid>
-          <Grid item xs={12} my={20}>
+
+      <Box sx={{ width: "80%", border: "4px solid green" }}>
+        <Grid container>
+          <Grid item xs={12} my={20} border="2px solid blue">
             <Box
               width="100%"
               display="flex"
@@ -100,15 +95,20 @@ const AboutUs = () => {
                 textAlign="justify"
               >
                 <Box maxWidth="80%">
-                  {" "}
                   <Typography variant="body1" color={theme.text}>
-                    Esta página es el producto final que surge como resultado de
-                    nuestro proceso de aprendizaje dentro del Coding Bootcamp,
-                    de Hack Academy. Como consigna se nos solicitó la
-                    implementación de un sitio e-commerce, para el cual el
-                    equipo tomó como temática la venta de muebles de diseño.
-                    Durante 3 arduas semanas, los 4 integrantes del equipo, le
-                    dedicaron mas de 150 hs de trabajo c/u.
+                    Esta página surge como resultado del proyecto final
+                    realizado en Hack Academy, en el marco de su Coding Bootcamp
+                    de full-stack Web Developer. El mismo consiste en un curso
+                    de fuerte carga teórico-práctica, con una duración de 3
+                    meses, el cual implica unas 600 horas de dedicación
+                    intensiva. Como consigna se nos solicitó la implementación
+                    de un sitio e-commerce, para el cual el equipo tomó como
+                    temática la mueblería de diseño. Se contó con un plazo de 3
+                    semanas para realizarlo y cada uno de los integrantes
+                    invirtió aproximadamente 150hs de trabajo. Cómo puntapié
+                    inicial tomamos un emprendimiento existente, llamado Reizen
+                    que, con el permiso de sus creadoras, parte de su branding
+                    fue utilizado para que el sitio se viera mas real.
                   </Typography>
                 </Box>
               </Box>
@@ -130,7 +130,7 @@ const AboutUs = () => {
               </Box>
             </Box>
           </Grid>
-          <Grid item xs={12} my={20}>
+          <Grid item xs={12} my={20} border="2px solid blue">
             <Box
               width="100%"
               display="flex"
@@ -165,31 +165,39 @@ const AboutUs = () => {
                   sx={{ lineHeight: "30px" }}
                   variant="body1"
                 >
-                  Layouts creados en figma, sacando ideas de páginas similares
-                  en cuanto a la temática y sus contenidos. A la hora de pasar
-                  todo esto al codigo, el equipo siempre estuvo en constante
-                  comunicacíon, mediante discord, herramienta fundamental que
-                  empleamos para así estar conectados y poder tomar decisiones.
-                  <br />
-                  Dividimos el proyecto en dos partes, back y front, comenzando
-                  por definir la estructura principal del backend, con sus
-                  respectivos endpoints/rutas, modelos y controladores, para
-                  luego conectarlo al front, este paso inicial fue entre todos
-                  como para tener una base donde desarrollar las diferentes
-                  partes por separado.
-                  <br />
-                  Optamos por la asignación de diferentes tareas para cada uno
-                  del equipo, así como también. en otras etapas. la
-                  implementación de perprograming, ya que en determinadas
-                  circunstancias ,alguno del equipo en particular, que se
-                  encontraba con mayor facilidad, para poder realiar alguna
-                  tarea en especifico, explicara para que todos pudiesemos
-                  aprender un poco mas durante la aplicación de código.
+                  Para el desarrollo de las ideas iniciales, el equipo optó por
+                  utilizar la herramienta Figma ya que con ella es fácil
+                  interactuar entre los integrantes en tiempo real.. En primer
+                  lugar, desarrollamos un MER (Modelo Entidad - Relación) Para
+                  tener claro cuáles serian las partes que formarían el proyecto
+                  y su estructura. Luego de esto se decidió la base de datos a
+                  utilizar, resultando la elegida, MongoDB. A partir de aquí los
+                  esquemas definidos fueron: Usuario, Producto, Categoría, Orden
+                  y Dirección de envío. Las ideas de diseño fueron tomadas de
+                  varios sitios similares en cuanto a temática y contenido
+                  tratando de, con cada parte, formar un todo armonioso. En
+                  cuanto a la metodología de trabajo utilizada encontramos en la
+                  herramienta "xxxxxxxxxxxxx GitHub" una buena forma de dividir
+                  y asignar las tareas, siguiendo la metodología SCRUM. A su
+                  vez, utilizamos Discord quién brinda una comunicación fluida
+                  entre los integrantes del equipo y con los instructores.
+                  Gracias a todo esto el proyecto fue creado y organizado de
+                  manera 100% remota. El trabajo fue dividido en dos partes, por
+                  un lado el código del back-end y por otro el del front-end.
+                  Para los cuales fueron creados dos repositorios en GitHub. Al
+                  comenzar a pasar las ideas a código, decidimos planear la
+                  estructura básica del back-end (rutas, modelos, controladore)
+                  entre todos, para luego poder conectar las distintas partes
+                  del front-end de manera individual y más fluída. El equipo
+                  trabajó muy cómodo desde este marco y el grupo humano fue muy
+                  contributivo, alegre y solidario entre si, lo que permitió
+                  llegar a un buen avance para cada Demo y a un mejor resultado
+                  final.
                 </Typography>
               </Box>
             </Box>
           </Grid>
-          <Grid item xs={12} my={20}>
+          <Grid item xs={12} my={20} border="2px solid blue">
             <Box
               width="100%"
               display="flex"
@@ -206,10 +214,13 @@ const AboutUs = () => {
               >
                 <Typography variant="h4">Proceso final</Typography>
                 <Typography variant="body1" color={theme.text}>
-                  Durante los ultimos dias del proyecto, se fueron puliendo
-                  funcionalidades como por ejemplo, las del carrito de compras,
-                  así como también, todo lo relacionado al responsive de las
-                  vistas, para una mejor experiencia del usuario.
+                  Durante los últimos días del desarrollo, se fueron puliendo
+                  funcionalidades, tales como la del carrito de compras y el
+                  login. A su vez se fueron adecuando las partes del front-end
+                  al diseño propuesto, se eligieron fuentes, íconos, imágenes,
+                  tamaño de los componentes, etc . lo cual implicó mucha
+                  dedicación en los detalles para que el proyecto sea responsive
+                  y para asegurar una mejor experiencia del usuario.
                 </Typography>
               </Box>
               <Box
@@ -231,7 +242,22 @@ const AboutUs = () => {
               </Box>
             </Box>
           </Grid>
-          <Grid item xs={12} my={20}>
+          <Grid item xs={12} my={20} border="2px solid blue">
+            <Box
+              width="100%"
+              display="flex"
+              flexWrap="wrap"
+              justifyContent="space-between"
+            >
+              <Typography variant="h4">MER</Typography>
+              <img
+                width="100%"
+                srcSet={require("../../assets/ecommerce-mer.png")}
+                alt="mer-reizen"
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12} my={20} border="2px solid blue">
             <Box
               width="100%"
               display="flex"
@@ -255,28 +281,20 @@ const AboutUs = () => {
                 textAlign="justify"
               >
                 <Typography variant="body1" color={theme.text}>
-                  Fue realmente una experiencia muy linda y gratificante,
-                  contentos del resultado que pudimos lograr. Muchas gracias por
-                  tomarse el tiempo de leer toda esta pequeña gran parte de lo
-                  que fue la experiencia de nuestro proyecto final. Esperemos
-                  que este sea el primero, de muchos proyectos por venir!
+                  Ha sido realmente una experiencia muy linda y gratificante y
+                  estamos muy contentos con el resultado logrado. Llegamos a la
+                  conclusión, luego de estos meses de trabajo intenso, de que no
+                  importa la cultura idioma o rama de experticia previa de cada
+                  uno, los lenguajes de programación son universales. Este
+                  proyecto no es sólo una unión de código, sino también de
+                  voluntades y de esfuerzo de cuatro personas con una meta en
+                  común: superarse y abrirse paso en una nueva etapa de sus
+                  vidas. Muchas gracias por tomarse el tiempo de leer esta
+                  pequeña reseña de lo que fue el proceso de nuestro proyecto
+                  final. Esperamos que éste sea el primero de muchos proyectos
+                  por venir. Feli Fer Fede Lu
                 </Typography>
               </Box>
-            </Box>
-          </Grid>
-          <Grid item xs={12} my={20}>
-            <Box
-              width="100%"
-              display="flex"
-              flexWrap="wrap"
-              justifyContent="space-between"
-            >
-              <Typography variant="h4">MER</Typography>
-              <img
-                width="100%"
-                srcSet={require("../../assets/ecommerce-mer.png")}
-                alt="mer-reizen"
-              />
             </Box>
           </Grid>
         </Grid>
