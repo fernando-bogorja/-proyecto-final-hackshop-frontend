8362eee8f823a351dc0f870746a0f6a72639ec67
elFonTii <feluu.contacto@gmail.com>
2022-07-07 23:50:42 -0300

    Checkout all done

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,8 +1,62 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { currentTheme as theme } from "../../theme";
+import { useNavigate } from "react-router-dom";
+
+const useRedirect = () => {
+  const [counter, setCounter] = React.useState(5);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(counter - 1);
+    }, 1000);
+    if (counter === 0) {
+      navigate("/");
+    }
+  }, [counter]);
+
+  return counter;
+};
+
 export default function NotFound() {
+  const counter = useRedirect();
   return (
-    <div className="not-found">
-      <h1>404</h1>
-      <p>Page not found</p>
-    </div>
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      width="100%"
+    >
+      <Box
+        minHeight="60%"
+        width={{ xs: "90%", sm: "60%", lg: "40%", xl: "35%" }}
+        mt={10}
+        textAlign="center"
+      >
+        <Typography
+          variant="h1"
+          fontFamily={theme.fonts.title}
+          fontWeight="bold"
+          fontSize={250}
+          color={theme.gray}
+          sx={{ letterSpacing: "40px" }}
+          mb={10}
+        >
+          404
+        </Typography>
+        <Typography
+          variant="h5"
+          color={theme.text}
+          fontFamily={theme.fonts.title}
+          fontWeight="bold"
+        >
+          Redireccionando en {counter} segundos
+        </Typography>
+      </Box>
+    </Box>
   );
 }
--- src/components/pages/Thanks.jsx
+++ src/components/pages/Thanks.jsx
@@ -1,154 +1,195 @@
+import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { currentTheme as theme } from "../../theme";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useParams, Navigate } from "react-router-dom";
+import axios from "axios";
+import global from "../../global";
+
+const useGetOrder = id => {
+  const [order, setOrder] = React.useState(null);
+
+  React.useEffect(() => {
+    axios
+      .get(`${global.api}/orders/${id}`)
+      .then(res => {
+        setOrder(res.data.order);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [id]);
+
+  return order;
+};
 
 export default function Thanks() {
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-      width="100%"
-      bgcolor={theme.gray}
-    >
-      <Box minHeight="60%" width="30%" bgcolor={theme.white} mt={10}>
+  const { orderId } = useParams();
+  const order = useGetOrder(orderId);
+
+  if (order) {
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+        width="100%"
+        bgcolor={theme.gray}
+      >
         <Box
-          width="100%"
-          p={2}
-          sx={{ borderBottom: `2px solid ${theme.gray}` }}
-        >
-          <Typography
-            variant="h6"
-            fontFamily={theme.fonts.title}
-            fontSize={25}
-            fontWeight="bold"
-            sx={{ letterSpacing: "2px" }}
-          >
-            Estado de la orden
-          </Typography>
-        </Box>
-        <Box
-          p={2}
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
+          minHeight="60%"
+          width={{ xs: "90%", sm: "60%", lg: "40%", xl: "35%" }}
+          bgcolor={theme.white}
+          mt={10}
         >
           <Box
+            width="100%"
+            p={2}
+            sx={{ borderBottom: `2px solid ${theme.gray}` }}
+          >
+            <Typography
+              variant="h6"
+              fontFamily={theme.fonts.title}
+              fontSize={25}
+              fontWeight="bold"
+              sx={{ letterSpacing: "2px" }}
+            >
+              Estado de la orden
+            </Typography>
+          </Box>
+          <Box
+            p={2}
             display="flex"
+            flexDirection="column"
             justifyContent="center"
             alignItems="center"
-            mt={5}
-          >
-            <FontAwesomeIcon
-              icon={faCircleCheck}
-              size="10x"
-              color={theme.green}
-            />
-          </Box>
-          <Box mt={3} textAlign="center" width="60%">
-            <Typography
-              variant="h4"
-              fontFamily={theme.fonts.title}
-              fontWeight="bold"
-            >
-              ¡Muchas gracias!
-            </Typography>
-            <Typography
-              variant="h4"
-              fontFamily={theme.fonts.title}
-              fontWeight="bold"
-            >
-              Tu orden está siendo procesada
-            </Typography>
-            <Typography variant="body1" color={theme.text} mt={2}>
-              Tu orden ha sido ingresada y está siendo procesada, podrás ver los
-              avances de tu orden en tu perfil, por cualquier duda o problema,
-              puedes contactarnos a través de nuestro email.
-            </Typography>
-            <Typography variant="body1" color={theme.black} mt={2}>
-              ID de la orden: 62c47d5bff1e5cf0021cf717
-            </Typography>
-          </Box>
-          <Box
-            mt={5}
-            width="90%"
-            sx={{ border: `1px solid ${theme.gray}`, borderRadius: "5px" }}
           >
             <Box
-              p={2}
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              sx={{ borderBottom: `2px solid ${theme.gray}` }}
-            >
-              <Typography variant="body1" color={theme.text}>
-                Pago total
-              </Typography>
-              <Typography variant="body1" color={theme.black} fontWeight="bold">
-                $100.00
-              </Typography>
-            </Box>
-            <Box
-              p={2}
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-            >
-              <Typography variant="body1" color={theme.text}>
-                Realizada el
-              </Typography>
-              <Typography variant="body1" color={theme.black} fontWeight="bold">
-                08/07/2022
-              </Typography>
-            </Box>
-          </Box>
-          <Box mt={3} width="90%">
-            <Box
-              width="100%"
               display="flex"
               justifyContent="center"
               alignItems="center"
-              bgcolor={theme.black}
-              sx={{ borderRadius: "5px" }}
-              mb={1}
+              mt={5}
             >
-              <Link
-                to="/"
-                className="link-none"
-                style={{ padding: "15px 150px" }}
+              <FontAwesomeIcon
+                icon={faCircleCheck}
+                size="10x"
+                color={theme.green}
+              />
+            </Box>
+            <Box mt={3} textAlign="center" width="60%">
+              <Typography
+                variant="h4"
+                fontFamily={theme.fonts.title}
+                fontWeight="bold"
               >
-                <Typography variant="body1" color={theme.white}>
-                  Volver a la tienda
-                </Typography>
-              </Link>
+                ¡Muchas gracias!
+              </Typography>
+              <Typography
+                variant="h4"
+                fontFamily={theme.fonts.title}
+                fontWeight="bold"
+              >
+                Tu orden está siendo procesada
+              </Typography>
+              <Typography variant="body1" color={theme.text} mt={2}>
+                Tu orden ha sido ingresada y está siendo procesada, podrás ver
+                los avances de tu orden en tu perfil, por cualquier duda o
+                problema, puedes contactarnos a través de nuestro email.
+              </Typography>
+              <Typography variant="body1" color={theme.black} mt={2}>
+                ID de la orden: {order._id}
+              </Typography>
             </Box>
             <Box
-              width="100%"
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              mb={1}
-              sx={{ borderRadius: "5px", border: `2px solid ${theme.text}` }}
+              mt={5}
+              width="90%"
+              sx={{ border: `1px solid ${theme.gray}`, borderRadius: "5px" }}
             >
-              <Link
-                to="/"
-                className="link-none"
-                style={{ padding: "12px 150px" }}
+              <Box
+                p={2}
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                sx={{ borderBottom: `2px solid ${theme.gray}` }}
               >
-                <Typography variant="body1" color={theme.black}>
-                  Ver mis ordenes
+                <Typography variant="body1" color={theme.text}>
+                  Pago total
                 </Typography>
-              </Link>
+                <Typography
+                  variant="body1"
+                  color={theme.black}
+                  fontWeight="bold"
+                >
+                  $ {order.total}
+                </Typography>
+              </Box>
+              <Box
+                p={2}
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <Typography variant="body1" color={theme.text}>
+                  Realizada el
+                </Typography>
+                <Typography
+                  variant="body1"
+                  color={theme.black}
+                  fontWeight="bold"
+                >
+                  {
+                    //format date
+                    new Date(order.createdAt).toLocaleDateString("es-AR", {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "numeric",
+                    })
+                  }
+                </Typography>
+              </Box>
+            </Box>
+            <Box mt={3} width="90%">
+              <Box
+                width="100%"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                bgcolor={theme.black}
+                sx={{ borderRadius: "5px" }}
+                mb={1}
+                p={2}
+              >
+                <Link to="/" className="link-none">
+                  <Typography variant="body1" color={theme.white}>
+                    Volver a la tienda
+                  </Typography>
+                </Link>
+              </Box>
+              <Box
+                width="100%"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                mb={1}
+                p={1.5}
+                sx={{ borderRadius: "5px", border: `2px solid ${theme.text}` }}
+              >
+                <Link to="/" className="link-none">
+                  <Typography variant="body1" color={theme.black}>
+                    Ver mis ordenes
+                  </Typography>
+                </Link>
+              </Box>
             </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
-  );
+    );
+  } else {
+    return <Navigate to="/not-found" />;
+  }
 }
